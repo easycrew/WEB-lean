d2a67bc837eb339615a6e12b7f729e31b16ac0fd
yangjingjing <jingjingyang@jieyuechina.com>
2020-01-15 15:52:00 +0800

    add m-header

---
+++ vue-music/src/components/m-header/m-header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="m-header">
+    <div class="icon"></div>
+    <h1 class="text">Chicken Music</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus" scoped>
+  .m-header
+    position:relative
+    height:44px
+    text-align:center
+    font-size:0
+    .icon
+      display:inline-block
+      vertical-align:top
+      margin-top:6px
+      width:30px
+      height:32px
+      margin-right:9px
+      b-image('logo')
+      background-size:30px 32px
+    .text
+      display:inline-block
+      vertical-top:top
+      line-height: 44px
+      font-size:$font-size-large
+</style>
--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -2,9 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import fastclick from 'fastclick'
 import router from './router'
 
 Vue.config.productionTip = false
+
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
